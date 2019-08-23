4bc920594d5828193107e0a5d5de891f63f22a9f
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-08-23 10:20:53 -0700

    append, no test

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,15 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    #add elements to buffer
+    #when the buffer is full, add the newest element and overwrite the oldest
+
+    #check capacity to current
+    if self.current >= len(self.storage):
+      #reset current to the start of the list if full
+      self.current = 0
+    self.storage[self.current] = item
+    self.current +=1
 
   def get(self):
     pass
